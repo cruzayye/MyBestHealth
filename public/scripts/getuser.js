e9d1d481bda2b662a59b49abc383b9600113902a
cruzayye <johnnyc010392@gmail.com>
2018-12-06 14:06:25 -0800

    dates js gets start and end of week aka monday through sunday

--- public/scripts/getuser.js
+++ public/scripts/getuser.js
@@ -23,6 +23,7 @@ function appendUser() {
 User.getUser = function () {
     if (localStorage.user) {
         User.loadAll(JSON.parse(localStorage.user));
+
         appendUser();
     } else
         //still need to fill in code for localStorage
@@ -52,34 +53,4 @@ init();
 
 
 
-    //well need this since it has a function if theres something in local storage. 
-// Article.fetchAll = function() {
-//     if (localStorage.rawData) {
-//       // When rawData is already in localStorage,
-//       // we can load it with the .loadAll function above,
-//       // and then render the index page (using the proper method on the articleView object).
-//       Article.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
-//       articleView.initIndexPage();
-//       console.log('string');
-//       //TODO: What method do we call to render the index page?
-//     } else {
 
-
-
-
-
-//         // TODO: When we don't already have the rawData,
-//         // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-//         // cache it in localStorage so we can skip the server call next time,
-//         // then load all the data into Article.all with the .loadAll function above,
-//         // and then render the index page.
-//         $.get('data/hackerIpsum.json', showFile);
-
-//         function showFile(response) {
-
-//         localStorage.setItem("rawData", JSON.stringify(response));
-//         Article.loadAll(response); 
-//         articleView.initIndexPage();
-//         }
-//       }
-//     }
