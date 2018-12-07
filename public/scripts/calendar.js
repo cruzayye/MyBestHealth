c7543409b1ee27591aa3d4618b371d0ccce97399
cruzayye <johnnyc010392@gmail.com>
2018-12-06 20:43:35 -0800

    still adjusting stats page

---
+++ public/scripts/calendar.js
@@ -0,0 +1,17 @@
+var Calendar = tui.Calendar;
+
+// var Calendar = require('tui-calendar'); /* CommonJS */
+
+// import Calendar from 'tui-calendar'; /* ES6 */
+
+$('#calendar').tuiCalendar({
+    defaultView: 'month',
+    taskView: true,
+    template: {
+      monthGridHeader: function(model) {
+        var date = new Date(model.date);
+        var template = '<span class="tui-full-calendar-weekday-grid-date">' + date.getDate() + '</span>';
+        return template;
+      }
+    }
+  });
